5a23dea4a2f2da3dd89f48c87d1e8aea048f4c92
alexandra_shmoylova@yahoo.com <alexandra_shmoylova@yahoo.com>
2023-01-27 14:28:00 +0400

    Add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,6 +17,12 @@
 const header = document.querySelector(".header");
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
+const modal = document.querySelector('.modal');
+const popup = document.querySelector('.modal__inner');
+const openPopupButton = document.querySelector('.intro__btn');
+const closePopupButton = document.querySelector('.modal__close');
+const form = document.getElementById('modal-form');
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
 header.classList.remove("header--nojs");
 
@@ -27,5 +33,46 @@ const openmenuNav = (evt) => {
 };
 
 burger.addEventListener("click", openmenuNav);
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const onPopupCloseButtonClick = () => {
+  closePopup();
+};
+
+const onWindowCloseByClick = (evt) => {
+  closePopupByClick(evt);
+};
+
+function closePopupByClick(evt) {
+  if (evt.target === modal) {
+    closePopup();
+  }
+}
+
+function closePopup() {
+  modal.classList.remove('modal--active');
+  popup.classList.remove('modal__inner--active');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  document.removeEventListener('click', onPopupCloseButtonClick);
+  document.removeEventListener('click', onWindowCloseByClick);
+  form.reset();
+}
+
+function openPopup(evt) {
+  evt.preventDefault();
+  modal.classList.add('modal--active');
+  popup.classList.add('modal__inner--active');
+  closePopupButton.addEventListener('click', onPopupCloseButtonClick);
+  document.addEventListener('keydown', onPopupEscKeydown);
+  document.addEventListener('click', onWindowCloseByClick);
+}
+
+openPopupButton.addEventListener('click', openPopup);
 
 console.log('Hello');
